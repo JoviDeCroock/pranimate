641ee71e762862a33839f1067aec535a52bb70d6
Jovi De Croock <decroockjovi@gmail.com>
2020-10-04 19:26:19 +0200

    add presets

--- src/useSpring.ts
+++ src/useSpring.ts
@@ -9,6 +9,7 @@ export interface UseSpringProps {
   duration: number;
   getValue?: (value: number) => string;
   infinite?: boolean;
+  preset?: 'wobbly' | 'noWobble' | 'stiff';
 }
 
 const lerp = (from: number, to: number, fraction: number) =>
@@ -22,6 +23,7 @@ export const useSpring = ({
   property,
   infinite,
   lazy,
+  preset,
 }: UseSpringProps): [Ref<HTMLElement>, (activated: boolean) => void] => {
   const from = propsFrom || 0;
   const [activated, setActivated] = useState(!lazy);
@@ -33,10 +35,18 @@ export const useSpring = ({
 
   useLayoutEffect(() => {
     if (activated) {
+      const start = reverse ? to : from;
       const target = reverse ? from : to;
       const tick = Math.abs(from - to) / duration;
+      let i = 0;
       intervalId.current = setInterval(() => {
-        value.current = lerp(value.current, target, tick);
+        i++;
+        value.current = lerp(
+          start,
+          target,
+          presets[preset || 'noWobble'](i / 100)
+        );
+
         requestAnimationFrame(() => {
           // @ts-ignore
           ref.current.style[property] = getValue
@@ -73,4 +83,18 @@ export const useSpring = ({
   }, [reverse, activated]);
 
   return [ref, setActivated];
+};
+
+const presets = {
+  noWobble: (t: number) =>
+    2.71828 ** (-13 * t) *
+    (2.71828 ** (13 * t) - 13 * Math.sin(t) - Math.cos(t)),
+  wobbly: (t: number) =>
+    -0.5 *
+    2.71828 ** (-6 * t) *
+    (-2 * 2.71828 ** (6 * t) + Math.sin(12 * t) + 2 * Math.cos(12 * t)),
+  stiff: (t: number) =>
+    Math.sqrt(10 / 11) * 2.71828 ** (-10 * t) * Math.sin(Math.sqrt(110) * t) -
+    2.71828 ** (-10 * t) * Math.cos(Math.sqrt(110) * t) +
+    1,
 };
