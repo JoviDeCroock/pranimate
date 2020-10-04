6c1df918e36286d160bd6cdbec8856abe6ce96ee
Jovi De Croock <decroockjovi@gmail.com>
2020-10-04 14:27:11 +0200

    Update useSpring.ts

--- src/useSpring.ts
+++ src/useSpring.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'preact/hooks';
+import { useState, useLayoutEffect, useRef } from 'preact/hooks';
 
 interface UseSpringProps {
   lazy?: boolean;
@@ -6,26 +6,63 @@ interface UseSpringProps {
   to: number;
   property: string;
   duration: number;
+  getValue?: (value: number) => string;
+  infinite?: boolean;
 }
 
 const lerp = (from: number, to: number, fraction: number) =>
   (to - from) * fraction + from;
 
 export const useSpring = ({
-  lazy,
-  from,
+  from: propsFrom,
   to,
   duration,
+  getValue,
   property,
-}: UseSpringProps) => {
-  const [isActivated, setIsActivated] = useState(!lazy);
-  const [value, setValue] = useState<number>(from || 0);
+  infinite,
+  lazy
+}: UseSpringProps): [Ref<HTMLElement>, (activated: boolean) => void] => {
+  const from = propsFrom || 0;
+  const [activated, setActivated] = useState(!lazy);
+  const [reverse, setReverse] = useState(false);
+  const ref = useRef<HTMLElement>();
+  const value = useRef(from);
 
-  useEffect(() => {
-    if (isActivated) {
-      setValue(lerp(value, to, to - (from || 0) / duration));
+  useLayoutEffect(() => {
+    if (activated) {
+      const target = reverse ? from : to;
+      const tick = Math.abs(from - to) / duration;
+      const intervalID = setInterval(() => {
+        value.current = lerp(value.current, target, tick);
+        if (value.current === target) {
+          clearInterval(intervalID);
+        } else {
+          requestAnimationFrame(() => {
+            ref.current.style[property] = getValue
+              ? getValue(value.current)
+              : value.current;
+          });
+        }
+      }, tick);
+
+      const timeoutID = setTimeout(() => {
+        clearInterval(intervalID);
+        requestAnimationFrame(() => {
+          ref.current.style[property] = getValue(target);
+          if (infinite) {
+            setReverse(!reverse);
+            value.current = reverse ? from : to;
+            console.log("setting value to", value.current);
+          }
+        });
+      }, duration);
+
+      return () => {
+        clearInterval(intervalID);
+        clearTimeout(timeoutID);
+      };
     }
-  }, [isActivated]);
+  }, [reverse, activated]);
 
-  return [{ [property]: value }, setIsActivated];
+  return [ref, setActivated];
 };
